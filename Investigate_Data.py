e01087e811a4c2336e0d2b5d51434687ab82beaf
juanmahvelez <juanmahvelez@gmail.com>
2014-04-23 20:57:09 -0700

    comment in script recommending a sampling workflow. modified stop_words

--- Investigate_Data.py
+++ Investigate_Data.py
@@ -1,10 +1,19 @@
 from gensim import corpora, models, similarities
 from gensim.models import hdpmodel, ldamodel
 
+# Below opens the file. As it stands, I'm just opening a selected number of review_lines.
+# I want to be able to select a random sample, but I think we want to have the same random sample.
+# I propose we do the following:
+	# 1. Open the file
+	# 2. Read every 11th line starting at row 9 (these are the reviews)
+	# 3. Save all the lines that we read as a separate file
+	# 4. Create a random sample from that file (we need to decide on size) and save that
+	# 5. We'll work on the file we created in step 4 so that we're always using the same random sample
+
 import itertools
 documents = []
 with open('../Arts.txt', 'r') as f:
-    review_lines = itertools.islice(f, 9, 1000, 11)
+    review_lines = itertools.islice(f, 9, 20000, 11)
     for line in review_lines:
     	documents.append(line[13:len(line)-1].lower())
 
@@ -29,7 +38,7 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # LDA
-lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=5)
+lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=10)
 corpus_lda = lda[corpus]
 
 #for l,t in itertools.izip(corpus_lda,corpus):
