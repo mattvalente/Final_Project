69691603be7f1394160976db604da23718fc7f06
mattvalente <mvalente760@gmail.com>
2014-05-06 12:21:46 -0700

    lda with time tests

--- Investigate_Data.py
+++ Investigate_Data.py
@@ -1,49 +1,87 @@
 from gensim import corpora, models, similarities
-from gensim.models import hdpmodel, ldamodel
-
-# Below opens the file. As it stands, I'm just opening a selected number of review_lines.
-# I want to be able to select a random sample, but I think we want to have the same random sample.
-# I propose we do the following:
-	# 1. Open the file
-	# 2. Read every 11th line starting at row 9 (these are the reviews)
-	# 3. Save all the lines that we read as a separate file
-	# 4. Create a random sample from that file (we need to decide on size) and save that
-	# 5. We'll work on the file we created in step 4 so that we're always using the same random sample
-
-import itertools
-documents = []
-with open('../Arts.txt', 'r') as f:
-    review_lines = itertools.islice(f, 9, 20000, 11)
-    for line in review_lines:
-    	documents.append(line[13:len(line)-1].lower())
+from gensim.models import hdpmodel, ldamodel, lsimodel
+import pickle
+import time
 
 
+
+#start = time.clock()
+reviews = []
+with open('../booktenthousand.txt', 'r') as f:
+	for line in f:
+		reviews.append(line)
+#end = time.clock()
+#print 'reviews!'
+#print 'time: ', (end -start)
+
+
+#start = time.clock()
 # remove stop words
 with open ('Stop_Words.txt', 'r') as myfile:
-    data=myfile.read().replace('\n', '')	
+    data=myfile.read().replace('\n', '')
 stop_list = set(data.split())
-#stop_list = set('for a of the and to in i they are these as all with my have on you that on was is we this were it so are not be but will do can'.split())
-texts = [[word for word in document.split() if word not in stop_list]
-for document in documents]
+texts = [[word for word in review.split() if word not in stop_list]			# splits words in each review and...
+for review in reviews]														# takes stop words out of each review
+																			# [['how now brown cow'], ['apple banana']] --> [['how', 'now', 'brown', 'cow'], ['apple', 'banana']]
+#end = time.clock()
+#print 'stopwords!'
+#print 'time: ', (end - start)
 
-#print stop_list
 
+#start = time.clock()
 # remove words that appear only once
-all_tokens = sum(texts, [])
+all_tokens = sum(texts, [])		# creates 1 list of all words in all reviews
 tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
 texts = [[word for word in text if word not in tokens_once]
          for text in texts]
+#end = time.clock()
+#print 'remove words!'
+#print 'time: ', (end - start)
 
+
+#start = time.clock()
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
+#end = time.clock()
+#print 'corpus!'
+#print 'time :', (end - start)
 
+
+#start = time.clock()
+# saves dictionary and corpus objects to file
+with open('../dfile.obj', 'w') as f:
+	pickle.dump(dictionary, f)
+with open('../cfile.obj', 'w') as g:
+	pickle.dump(corpus, g)
+#end = time.clock()
+#print 'pickle yeah!'
+#print 'time: ', (end - start)
+
+
+
+# saves dictionary and corpus objects for later use
+with open('../dfile.obj', 'r') as f:
+	dictionary = pickle.load(f)
+with open('../cfile.obj', 'r') as g:
+	corpus = pickle.load(g)
+
+#print 'pickled!'
 # LDA
+#start = time.clock()
 lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=10)
-corpus_lda = lda[corpus]
+#end = time.clock()
+#print 'lda time: ', (end - start)
 
-#for l,t in itertools.izip(corpus_lda,corpus):
-  #print l,"#",t
 
-for i in range(0, lda.num_topics-1):
+# saves lda model
+lda.save('ldamodel')
+
+
+#lda = ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=3)
+#corpus_lda = lda[corpus]
+
+
+'''
+for i in range(0, lda.num_topics):
 	print lda.print_topic(i)
-
+'''
